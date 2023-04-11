61f46860d37ab7631481ba39c3a49626abf8d11f
a-starovoytov <alexander.starovoytov@ecrt.ru>
2023-04-11 19:39:32 +0300

    /+/ add hello_world project
    Add project "hello_world" with first step of 02_homework (build .exe file)

---
+++ 2_hello_world/example.cpp
@@ -0,0 +1,16 @@
+#include "example.h"
+
+#include <iostream>
+
+void example_function() {
+	std::cout << "Hello from example function!" << std::endl;
+}
+void example_function_2() {
+	std::cout << "Hello from example function!" << std::endl;
+}
+void example_function_3() {
+	std::cout << "Hello from example function!" << std::endl;
+}
+void example_function_4() {
+	std::cout << "Hello from example function!" << std::endl;
+}
---
+++ 2_hello_world/example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void example_function();
+
+
+void examole_function_2();
+void examole_function_3();
+void examole_function_4();
